51f6c5d6eb6ca0ead67648102f9fe3b37f438e7b
GANESH W <155592898+ganeshwhere@users.noreply.github.com>
2024-03-04 23:38:20 +0530

    Update file_knowledge_tool.py
    I created a headers attribute in the FileKnowledgeTool class, so that it doesn't need to be redefined every time the _file_knowledge_search_api method is called.

--- core/tools/knowledge/file_knowledge_tool.py
+++ core/tools/knowledge/file_knowledge_tool.py
@@ -1,17 +1,13 @@
-# Standard Library
-import json
 import os
-
-# Third Party
+import json
 import requests
 
 VECTOR_DB_HOST = os.getenv("VECTOR_DB_HOST", "localhost")
 VECTOR_DB_MATCH_URL = f"http://{VECTOR_DB_HOST}:8010/similarity_match"
 
-
 class FileKnowledgeTool:
     name = "FileKnowledge"
-    description = "Useful for search knowledge or information from user's file"
+    description = "Useful for searching knowledge or information from user's files"
     parameters = {
         "type": "object",
         "properties": {
@@ -23,28 +19,29 @@ class FileKnowledgeTool:
         "required": ["query"],
     }
 
+    def __init__(self):
+        self.headers = {"accept": "application/json", "Content-Type": "application/json"}
+
     async def _arun(self, query: str, assistant_id):
-        return await file_knowledge_search_api(query, assistant_id)
+        return await self._run(query, assistant_id)
 
     def _run(self, query: str, assistant_id):
-        return file_knowledge_search_api(query, assistant_id)
+        return self._file_knowledge_search_api(query, assistant_id)
 
+    def _file_knowledge_search_api(self, query: str, assistant_id: str):
+        data = json.dumps(
+            {
+                "text": query,
+                "collection_name": assistant_id,
+                "topk": 10,
+                "rerank": False,
+                "topr": 5,
+            }
+        )
 
-def file_knowledge_search_api(query: str, assistant_id: str):
-    headers = {"accept": "application/json", "Content-Type": "application/json"}
-    data = json.dumps(
-        {
-            "text": query,
-            "collection_name": assistant_id,
-            "topk": 10,
-            "rerank": False,
-            "topr": 5,
-        }
-    )
-
-    response = requests.post(VECTOR_DB_MATCH_URL, headers=headers, data=data)
-    res = response.json()["response"]
-    txt = ""
-    for r in res:
-        txt += r["text"] + "\n\n"
-    return txt
+        response = requests.post(VECTOR_DB_MATCH_URL, headers=self.headers, data=data)
+        res = response.json()["response"]
+        txt = ""
+        for r in res:
+            txt += r["text"] + "\n\n"
+        return txt
